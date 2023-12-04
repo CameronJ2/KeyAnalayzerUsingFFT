b4c9dd26d9a04cb9a15c7f1d55b9bb28123473af
CameronJ2 <jonescameron980p@gmail.com>
2023-12-04 10:28:24 -0600

    Working deliverable

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ FILE_PATH = "C_major_scale_cut.wav"
 MIN_FREQ = 0 #first note A0 is 27.50, so anything below is just noise
 MAX_FREQ = 5000
 NOTES = {
-    "A0": [27.50, 30.867], "B0" : [30.868, 32.702], "C1" : [32.703,36.707], "D1" : [36.708, 41.202], "E1" : [41.203, 43.653], "F1" : [43.654, 48.998], "G1" : [48.999, 54.999], "A1" : [55.0, 61.734], "B1" : [61.735, 65.405], "C2" : [65.406, 73.415], "D2" : [73.416, 82.406], "E2" : [82.407, 87.306], "F2" : [87.307, 97.998], "G2" : [97.999, 109.99], "A2" : [110.00, 123.46], "B2" : [123.47, 130.80], "C3" : [130.81, 146.82], "D3" : [146.83, 164.80], "E3" : [164.81, 174.60], "F3" : [174.61, 195.99], "G3" : [196.00, 210.99], "A3" : [220.00, 246.93], "B3" : [246.94, 261.62], "C4" : [261.63, 293.66], "D4" : [293.67, 329.62], "E4" : [329.63, 249.22], "F4" : [349.23, 391.99], "G4" : [392.00, 439.99], "A4" : [440.00, 493.87], "B4" : [493.88, 523.24], "C5" : [523.25, 587.32], "D5" : [587.33, 659.25], "E5" : [659.26, 698.45], "F5" : [698.46, 783.98], "G5" : [783.99, 878.99], "A5" : [880.00, 987.76], "B5" : [987.77, 1046.49], "C6" : [1046.50, 1174.69], "D6" : [1174.70, 1318.49], "E6" : [1318.50, 1396.89], "F6" : [1396.9, 1567.99], "G6" : [1568.00, 1759.99], "A6" : [1760.00, 1975.49], "B6" : [1975.00, 2092.99], "C7" : [2093.00, 2349.29], "D7" : [2349.30, 2636.99], "E7" : [2637.00, 2792.99], "F7" : [2793.00, 3135.99], "G7" : [3136.00, 3519.99], "A7" : [3520.00, 3951.09], "B7" : [3951.10, 4185.99], "C8" : [4186.00, 5000.00]
+    "A0": 27.50, "B0" : 30.868, "C1" : 32.703, "D1" : 36.708, "E1" : 41.203, "F1" : 43.654, "G1" : 48.999, "A1" : 55.0, "B1" : 61.735, "C2" : 65.406, "D2" : 73.416, "E2" : 82.407, "F2" : 87.307, "G2" : 97.999, "A2" : 110.00, "B2" : 123.47, "C3" : 130.81, "D3" : 146.83, "E3" : 164.81, "F3" : 174.61, "G3" : 196.00, "A3" : 220.00, "B3" : 246.94, "C4" : 261.63, "D4" : 293.67, "E4" : 329.63, "F4" : 349.23, "G4" : 392.00, "A4" : 440.00, "B4" : 493.88, "C5" : 523.25, "D5" : 587.33, "E5" : 659.26, "F5" : 698.46, "G5" : 783.99, "A5" : 880.00, "B5" : 987.77, "C6" : 1046.50, "D6" : 1174.70, "E6" : 1318.50, "F6" : 1396.9, "G6" : 1568.00, "A6" : 1760.00, "B6" : 1975.00, "C7" : 2093.00, "D7" : 2349.30, "E7" : 2637.00, "F7" : 2793.00, "G7" : 3136.00, "A7" : 3520.00, "B7" : 3951.10, "C8" : 4186.00
 }
+
 AUDIO_FILE = "C_major_scale_cut.wav"
 FFT_PORTIONS = 0.25 #Each FFT calculation will use .25 seconds of audio.
 
@@ -50,40 +51,28 @@ for portion_idx in range(NUM_PORTIONS):
     max_frequency_over_time.append(max_amplitude_freq)
     # Check if the last four values are the same
     if (portion_idx > 4):
-        if (max_frequency_over_time[-1] == max_frequency_over_time[-2] == max_frequency_over_time[-3] == max_frequency_over_time[-4] and max_frequency_over_time[-1] > 20):
+        if (max_frequency_over_time[-1] == max_frequency_over_time[-2] and max_frequency_over_time[-1] > 20):
             if (prev_value != max_frequency_over_time[-1]):
-                print("4 frequencies found in sequence: {}".format(max_frequency_over_time[-1]))
+                #print("4 frequencies found in sequence: {}".format(max_frequency_over_time[-1]))
                 frequencies_played.append(max_frequency_over_time[-1])
             prev_value = max_frequency_over_time[-1]
     
 valid_notes = []
 for freq in frequencies_played:
-    for note, freq_range in NOTES.items():
-        if freq_range[0] <= freq <= freq_range[1]:
-            valid_notes.append(note)
-            break
-       
-"""for i in range(len(valid_notes) - 1):
-        current_note = valid_notes[i]
-        next_note = valid_notes[i + 1]
-        if current_note != next_note:
-            print(current_note) """
+    # Find the closest note based on the absolute difference
+    closest_note = min(NOTES.items(), key=lambda x: abs(x[1] - freq))[0]
+    valid_notes.append(closest_note)
 
+for i in range(len(valid_notes) - 1):
+    current_note = valid_notes[i]
+    next_note = valid_notes[i + 1]
 
-"""     # Find the frequency with the highest amplitude in the valid range
-    max_amplitude_index = np.argmax(np.abs(fft_result[valid_freq_indices]))
-    max_amplitude_freq = frequencies[valid_freq_indices][max_amplitude_index]
-
-    # Print or store the result for the current portion
-    #print(f"Portion {(portion_idx + 1) * .25}: Highest Amplitude Frequency = {max_amplitude_freq} Hz")
-    #max_frequency_over_time.append(max_amplitude_freq)
-
-    #find the frequencies above the white noise threshold
-
-
-    # Check if the last four values are the same
-    if (portion_idx > 4):
-        if (max_frequency_over_time[-1] == max_frequency_over_time[-2] == max_frequency_over_time[-3] == max_frequency_over_time[-4] and max_frequency_over_time[-1] > 20):
-            if (prev_value != max_frequency_over_time[-1]):
-                print("4 frequencies found in sequence: {}".format(max_frequency_over_time[-1]))
-            prev_value = max_frequency_over_time[-1] """
+for i in range(len(valid_notes)):
+    if ((i + 1) % 10 == 1 and i+1 != 11):
+        print("{}st note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
+    elif ((i + 1) % 10 == 2 and i+1 != 12):
+        print("{}nd note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
+    elif ((i + 1) % 10 == 3 and i+1 != 13):
+        print("{}rd note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
+    else:
+        print("{}th note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-    # Check if the last four values are the same
-    prev_values.append(max_amplitude_freq)
-    if len(prev_values) == 4 and all(val == prev_values[0] for val in prev_values):
-        print(f"The last four values are the same: {prev_values[0]}")
-
-    # Remove the oldest value if more than four values are stored
-    if len(prev_values) > 4:
-        prev_values.pop(0)
+valid_notes
