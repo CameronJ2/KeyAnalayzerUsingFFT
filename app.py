feeec346fd1d6a5d333f1e4d1cb2a00af320798c
CameronJ2 <jonescameron980p@gmail.com>
2023-12-08 21:35:39 -0600

    Final turn in

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 import numpy as np
 from scipy.io import wavfile
@@ -31,16 +30,15 @@ FFT_PORTIONS = 0.25 #Each FFT calculation will use .25 seconds of audio.
 fs, fullData = wavfile.read(FILE_PATH)
 data = fullData[:, 0] #get just the first track if the audio is multi-track
 
-AUDIO_LENGTH = len(data) / fs #samples / sample rate = seconds
-
-
 # Now that we have our data, we need to process it in chunks. Since FFT does not have a time axis, we have to make many FFT calculations at a set interval throughout the audio file to see multiple frequencies. We want the frequency with the highest amplitude for each portion
 
 FFT_SAMPLES_PER_PORTION = FFT_PORTIONS * fs 
 NUM_PORTIONS = int(np.floor(len(data) / FFT_SAMPLES_PER_PORTION))
 max_frequency_over_time = []
+max_amplitude_over_time = [] 
 prev_value = "0"
 frequencies_played = []
+valid_notes = []
 
 for portion_idx in range(NUM_PORTIONS):
     start_idx = int(portion_idx * FFT_SAMPLES_PER_PORTION)
@@ -62,33 +60,30 @@ for portion_idx in range(NUM_PORTIONS):
 
     # Print or store the result for the current portion
     max_frequency_over_time.append(max_amplitude_freq)
+    max_amplitude_over_time.append(np.abs(fft_result[max_amplitude_index]))
     
 
 
     # Check if the last four values are the same
     if (portion_idx > 4):
-        if (max_frequency_over_time[-1] == max_frequency_over_time[-2] == max_frequency_over_time[-3] == max_frequency_over_time[-4] and max_frequency_over_time[-1] > 20):
+        if (max_frequency_over_time[-1] == max_frequency_over_time[-2] == max_frequency_over_time[-3] == max_frequency_over_time[-4] and max_frequency_over_time[-1] > 20 and max_amplitude_over_time[-1] > 5000):
             if (prev_value != max_frequency_over_time[-1]):
                 #print("4 frequencies found in sequence: {}".format(max_frequency_over_time[-1]))
                 frequencies_played.append(max_frequency_over_time[-1])
             prev_value = max_frequency_over_time[-1]
     
-valid_notes = []
+
 for freq in frequencies_played:
     # Find the closest note based on the absolute difference
     closest_note = min(NOTES.items(), key=lambda x: abs(x[1] - freq))[0]
     valid_notes.append(closest_note)
 
-for i in range(len(valid_notes) - 1):
-    current_note = valid_notes[i]
-    next_note = valid_notes[i + 1]
-
 for i in range(len(valid_notes)):
     if ((i + 1) % 10 == 1 and i+1 != 11):
-        print("{}st note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
+        print("{}st highest note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
     elif ((i + 1) % 10 == 2 and i+1 != 12):
-        print("{}nd note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
+        print("{}nd highest note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
     elif ((i + 1) % 10 == 3 and i+1 != 13):
-        print("{}rd note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
+        print("{}rd highest note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
     else:
-        print("{}th note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
+        print("{}th highest note played: {} at frequency {}".format(i + 1, valid_notes[i], frequencies_played[i]))
